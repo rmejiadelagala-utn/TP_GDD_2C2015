3502eef000ea9c31c0840c376062aabf0818c334
rmejiadelagala-utn <rmejiadelagala.utn@gmail.com>
2015-11-21 12:16:53 -0300

    agrego procedimiento para obtener dataset de viajes

--- SQL_SERVER/script_creacion_objetos.sql
+++ SQL_SERVER/script_creacion_objetos.sql
@@ -10,6 +10,8 @@ IF OBJECT_ID('SFX.ModificarCiudad') IS NOT NULL
 DROP PROCEDURE SFX.ModificarCiudad
 IF OBJECT_ID('SFX.BajaCiudad') IS NOT NULL
 DROP PROCEDURE SFX.BajaCiudad
+IF OBJECT_ID('SFX.GetViajesDisponibles') IS NOT NULL
+DROP PROCEDURE SFX.BajaCiudad
 
 
 /****** Object:  UserDefinedFunction [sfx].[ExisteUsuario]    Script Date: 07/11/2015 10:39:20 a.m. ******/
@@ -131,4 +133,30 @@ BEGIN CATCH
 	PRINT ERROR_MESSAGE();
 
 END CATCH
+GO
+
+CREATE PROCEDURE [SFX].[GetViajesDisponibles] 
+	-- Add the parameters for the stored procedure here
+	@CiudadOrigId nvarchar(100),
+	@CiudadDestId nvarchar(100),
+	@FechaVuelo nvarchar(100)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	DECLARE @varfecha datetime;
+	SET NOCOUNT ON;
+	SET @varfecha = (select CAST(@FechaVuelo as datetime));
+    -- Insert statements for procedure here
+	select Via_Fecha_Salida, Via_Fecha_Llegada_Estimada,
+	       Aer_Matricula,Aer_KG_Disponibles, Rut_Cia_ID_Origen,Rut_Cia_ID_Destino, Cia_Descripcion
+                                                       from SFX.t_viajes,SfX.t_rutas,SFX.t_aeronaves, SFX.t_ciudades_aeropuertos
+                                                      where Via_Rut_ID=Rut_ID
+                                                      and Via_Aer_ID=Aer_ID 
+													  and Rut_Cia_ID_Origen=Cia_ID
+                                                      and Via_Fecha_Salida > CONVERT(datetime,@varfecha,121)  
+                                                     and Rut_Cia_ID_Origen = @CiudadOrigId
+                                                     and Rut_Cia_ID_Destino =@CiudadDestId;
+END;
+
 GO
